fbeafffc8b107ecb23ae6a9d7fd93157d2feedae
Soul-Decoder <ajgarciao95@gmail.com>
2025-06-30 17:55:15 -0500

    checkbox

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ import plotly.express as px
 import streamlit as st
      
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
-hist_button = st.button('Construir histograma') # crear un botón 
-if hist_button: # al hacer clic en el botón
+# crear una casilla de verificación
+build_histogram = st.checkbox('Construir un histograma')
+if build_histogram: # al hacer clic en el botón
     # escribir un mensaje
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')     
     # crear un histograma
