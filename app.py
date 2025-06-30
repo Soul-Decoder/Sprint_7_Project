70a7c439e9cf9a4a13148b229904673c804a6c28
Soul-Decoder <ajgarciao95@gmail.com>
2025-06-30 18:04:33 -0500

    checkbox with header

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
-     
+
+st.header('Visualización de datos de anuncios de venta de coches')
+
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 # crear una casilla de verificación
 build_histogram = st.checkbox('Construir un histograma')
@@ -10,5 +12,15 @@ if build_histogram: # al hacer clic en el botón
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')     
     # crear un histograma
     fig = px.histogram(car_data, x="odometer")
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+# crear una casilla de verificación
+build_scatter = st.checkbox('Construir un gráfico de dispersión')
+if build_scatter: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')     
+    # crear un histograma
+    fig = px.scatter(car_data, x="odometer", y="price") # crear un gráfico de dispersión
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
