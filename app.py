394fbe407ee9c1417642a9c8fee94c4b47745cab
Soul-Decoder <ajgarciao95@gmail.com>
2025-06-30 21:30:17 -0500

    plotly position title

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 #Encabezado
-st.header('Visualización de datos de anuncios de venta de coches')
+st.header('Visualización de datos de anuncios de venta de coches.')
 #Instrucciones
 st.write('Elija la que información desea visualizar:')
 #Lectura de datos
@@ -13,15 +13,18 @@ build_histogram = st.checkbox('Histograma: Distancia recorrida')
 #Condicional de la casilla de verificación
 if build_histogram:
     #Información de la elección
-    st.write('La opción Histograma fue seleccionada.\nA continuación, se presenta el gráfico...')     
+    st.write('La opción "Histograma" fue seleccionada.\nA continuación, se presenta el gráfico...')     
     #Se genera un histograma con 'plotly'
     fig = px.histogram(car_data, x="odometer", labels={"odometer": "Distancia recorrida (Millas)"})
     #Se definen los títulos del histograma
     #Se define la etiqueta del eje 'y' del histograma
-    fig.update_layout(title_text="Distribución de la distancia total (Millas)",
-    title_subtitle_text="La distribución se concentra entre las 0 y 0.3 Millas",
-    title_subtitle_font_size=12,
-    yaxis_title="Frecuencia"
+    fig.update_layout(
+        title_text="Distribución de la distancia total (Millas)",
+        title_font_size=20,
+        title_x=0.25,
+        title_subtitle_text="La distribución se concentra entre las 0 y 0.3 M",
+        title_subtitle_font_size=18,
+        yaxis_title="Frecuencia"
     )
     #Se muestra el gráfico interactivo
     st.plotly_chart(fig, use_container_width=True)
@@ -31,7 +34,7 @@ build_scatter = st.checkbox('Gráfico de dispersión: Distancia total y precio')
 #Condicional de la casilla de verificación
 if build_scatter:
     #Título del gráfico de dispersión
-    st.write('El gráfico de dispersión fue seleccionado.\nA continuación, se presenta el gráfico...')
+    st.write('La opción "Gráfico de dispersión" fue seleccionado.\nA continuación, se presenta el gráfico...')
     #Se calcula el coeficiente de correlación
     coe_cor=car_data["odometer"].corr(car_data["price"])    
     #Se genera un gráfico de dispersión con 'plotly'
@@ -39,8 +42,10 @@ if build_scatter:
     #Se definen los títulos del gráfico de dispersión
     fig.update_layout(
         title_text="Relación entre la distancia total del vehículo y su precio",
+        title_font_size=20,
+        title_x=0.2,
         title_subtitle_text="Correlación negativa: {:.2f}".format(coe_cor),
-        title_subtitle_font_size=12
+        title_subtitle_font_size=18
         )
     #Se muestra el gráfico interactivo
     st.plotly_chart(fig, use_container_width=True)
