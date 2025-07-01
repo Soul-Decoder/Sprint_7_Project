e97eb9c5e14dd59ed0bea2e4e787b5f39d2f854e
Soul-Decoder <ajgarciao95@gmail.com>
2025-06-30 20:18:12 -0500

    python file comments

--- app.py
+++ app.py
@@ -1,26 +1,31 @@
+#Importación de librerías
 import pandas as pd
 import plotly.express as px
 import streamlit as st
-
+#Encabezado
 st.header('Visualización de datos de anuncios de venta de coches')
-
-car_data = pd.read_csv('vehicles_us.csv') # leer los datos
-# crear una casilla de verificación
-build_histogram = st.checkbox('Construir un histograma')
-if build_histogram: # al hacer clic en el botón
-    # escribir un mensaje
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')     
-    # crear un histograma
+#Instrucciones
+st.write('A continuación elija que información que desea visualizar:')
+#Lectura de datos
+car_data = pd.read_csv('vehicles_us.csv') 
+#Casilla de verificación para el histograma
+build_histogram = st.checkbox('Histograma: Distancia recorrida (Millas)')
+#Condicional de la casilla de verificación
+if build_histogram:
+    #Título del histograma
+    st.write('Distribución de la distancia total (Millas)')     
+    #Se genera un histograma con 'plotly'
     fig = px.histogram(car_data, x="odometer")
-    # mostrar un gráfico Plotly interactivo
+    #Se muestra el gráfico interactivo
     st.plotly_chart(fig, use_container_width=True)
 
-# crear una casilla de verificación
-build_scatter = st.checkbox('Construir un gráfico de dispersión')
-if build_scatter: # al hacer clic en el botón
-    # escribir un mensaje
-    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')     
-    # crear un histograma
-    fig = px.scatter(car_data, x="odometer", y="price") # crear un gráfico de dispersión
-    # mostrar un gráfico Plotly interactivo
+#Casilla de verificación para el histograma
+build_scatter = st.checkbox('Gráfico de dispersión: Distancia total y precio')
+#Condicional de la casilla de verificación
+if build_scatter:
+    #Título del gráfico de dispersión
+    st.write('Relación entre la distancia total del vehículo y su precio')     
+    #Se genera un gráfico de dispersión con 'plotly'
+    fig = px.scatter(car_data, x="odometer", y="price")
+    #Se muestra el gráfico interactivo
     st.plotly_chart(fig, use_container_width=True)
